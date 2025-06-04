2d4c7c6c488cb96f5468073721d5d0d411b21bf3
Zidan <maulanazidan12@gmail.com>
2025-06-04 14:54:38 +0700

    feat: update loading in project

--- src/app/(admin)/review/page.jsx
+++ src/app/(admin)/review/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { ExternalLink, Star } from "lucide-react";
+import { ExternalLink, Loader, Star } from "lucide-react";
 import {
   AlertDialog,
   AlertDialogTrigger,
@@ -16,18 +16,21 @@ import {
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
+import { set } from "zod";
 
 const AllProjectsPage = () => {
   const [projects, setProjects] = useState([]);
   const [selectedProjectId, setSelectedProjectId] = useState(null);
   const [rating, setRating] = useState(0);
   const [token, setToken] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const router = useRouter();
 
   // Fetch projects with optional token
   const fetchDataWithToken = async (token) => {
     try {
+      setLoading(true);
       const headers = token
         ? {
             Authorization: `Bearer ${token}`,
@@ -48,7 +51,9 @@ const AllProjectsPage = () => {
       const json = await response.json();
       setProjects(json.data.projects);
     } catch (error) {
-      Swal.fire("Gagal", error.message, "error")
+      Swal.fire("Gagal", error.message, "error");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -79,147 +84,155 @@ const AllProjectsPage = () => {
       });
 
       if (!res.ok) throw new Error("Gagal mengirim penilaian");
-      Swal.fire("And BOOOOM!!!", "This projek telah di rating", "success")
+      Swal.fire("And BOOOOM!!!", "This projek telah di rating", "success");
       // Refresh project data after rating
       fetchDataWithToken(token);
       setSelectedProjectId(null);
       setRating(0);
     } catch (error) {
-      Swal.fire("Gagal", error.message, "error")
+      Swal.fire("Gagal", error.message, "error");
     }
   };
 
   return (
     <div className="min-h-screen bg-[#f8fafc] text-[#1e293b] py-12 px-6 md:px-20">
-      <h1 className="text-3xl md:text-4xl font-bold mb-12 text-center uppercase">
+      <h1 className="text-3xl md:text-4xl font-bold mb-12 text-center uppercase text-primary">
         Semua Proyek Peserta
       </h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {projects.map((proj, idx) => (
-          <div
-            key={idx}
-            className="bg-white border border-gray-200 rounded-2xl shadow-md overflow-hidden flex flex-col hover:shadow-xl transition duration-300"
-          >
-            <div className="overflow-hidden">
-              <img
-                src={proj.image_project_link}
-                alt={`Preview ${proj.name_mhs}`}
-                className="h-48 w-full object-cover transform hover:scale-105 transition duration-500"
-                
-              />
-            </div>
-            <div className="p-5 flex flex-col justify-between flex-1">
-              <div>
-                <div className="flex items-center justify-between mb-2">
-                  <h2 className="text-lg font-bold">{proj.name_mhs}</h2>
-                  <span
-                    className={`text-xs font-semibold px-3 py-1 rounded-full ${
-                      proj.status === "Selesai"
-                        ? "bg-green-100 text-green-600"
-                        : "bg-yellow-100 text-yellow-600"
-                    }`}
-                  >
-                    {proj.status}
-                  </span>
-                </div>
-                <p className="text-sm text-gray-600 mb-2">NIM: {proj.nim_mhs}</p>
-
-                <div className="space-y-1 mb-3">
-                  <a
-                    href={proj.project_link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-flex items-center text-blue-600 hover:underline text-sm"
-                  >
-                    <ExternalLink className="w-4 h-4 mr-1" /> Website
-                  </a>
-                  <br />
-                  <a
-                    href={proj.profile_link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-flex items-center text-purple-600 hover:underline text-sm"
-                  >
-                    <ExternalLink className="w-4 h-4 mr-1" /> Profile
-                  </a>
-                </div>
+      {loading ? (
+        <div className="mt-20 flex flex-col justify-center items-center gap-4 text-center text-xl text-primary animate-pulse">
+          <Loader size={32} className="animate-spin text-primary" />
+          <p>Mengambil Project</p>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((proj, idx) => (
+            <div
+              key={idx}
+              className="bg-white border border-gray-200 rounded-2xl shadow-md overflow-hidden flex flex-col hover:shadow-xl transition duration-300"
+            >
+              <div className="overflow-hidden">
+                <img
+                  src={proj.image_project_link}
+                  alt={`Preview ${proj.name_mhs}`}
+                  className="h-48 w-full object-cover transform hover:scale-105 transition duration-500"
+                />
               </div>
-
-              <div className="mt-2 flex items-center text-yellow-500 gap-1">
-                {[...Array(5)].map((_, i) => (
-                  <Star
-                    key={i}
-                    className={`w-4 h-4 ${
-                      i < Math.floor(proj.average_score)
-                        ? "fill-yellow-400"
-                        : "stroke-yellow-400"
-                    }`}
-                  />
-                ))}
-                <span className="text-sm ml-2 text-gray-500">
-                  ({proj.average_score})
-                </span>
-
-                {token !== "" && proj.userReview?.reviewed ? (
-                  <span className="ml-auto text-sm font-semibold text-green-700">
-                    Anda telah mereview, score: {proj.userReview.score}
-                  </span>
-                ) : null}
-
-                {/* Show button if user has token and has not reviewed */}
-                {token !== "" && !proj.userReview?.reviewed && (
-                  <div className="ml-auto">
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button
-                          variant="default"
-                          className="text-sm px-3 py-1"
-                          onClick={() => {
-                            setSelectedProjectId(proj.id_project);
-                            setRating(0);
-                          }}
-                        >
-                          Bintangi
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>Beri Penilaian</AlertDialogTitle>
-                          <AlertDialogDescription>
-                            Silakan berikan rating untuk proyek ini.
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-
-                        <div className="flex justify-center gap-2 py-4">
-                          {[...Array(5)].map((_, i) => (
-                            <Star
-                              key={i}
-                              onClick={() => setRating(i + 1)}
-                              className={`w-6 h-6 cursor-pointer transition ${
-                                i < rating
-                                  ? "fill-yellow-400 stroke-yellow-400"
-                                  : "stroke-yellow-400"
-                              }`}
-                            />
-                          ))}
-                        </div>
-
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Batal</AlertDialogCancel>
-                          <AlertDialogAction onClick={() => handleSubmit()}>
-                            Kirim
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
+              <div className="p-5 flex flex-col justify-between flex-1">
+                <div>
+                  <div className="flex items-center justify-between mb-2">
+                    <h2 className="text-lg font-bold">{proj.name_mhs}</h2>
+                    <span
+                      className={`text-xs font-semibold px-3 py-1 rounded-full ${
+                        proj.status === "Selesai"
+                          ? "bg-green-100 text-green-600"
+                          : "bg-yellow-100 text-yellow-600"
+                      }`}
+                    >
+                      {proj.status}
+                    </span>
                   </div>
-                )}
+                  <p className="text-sm text-gray-600 mb-2">
+                    NIM: {proj.nim_mhs}
+                  </p>
+
+                  <div className="space-y-1 mb-3">
+                    <a
+                      href={proj.project_link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-flex items-center text-blue-600 hover:underline text-sm"
+                    >
+                      <ExternalLink className="w-4 h-4 mr-1" /> Website
+                    </a>
+                    <br />
+                    <a
+                      href={proj.profile_link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-flex items-center text-purple-600 hover:underline text-sm"
+                    >
+                      <ExternalLink className="w-4 h-4 mr-1" /> Profile
+                    </a>
+                  </div>
+                </div>
+
+                <div className="mt-2 flex items-center text-yellow-500 gap-1">
+                  {[...Array(5)].map((_, i) => (
+                    <Star
+                      key={i}
+                      className={`w-4 h-4 ${
+                        i < Math.floor(proj.average_score)
+                          ? "fill-yellow-400"
+                          : "stroke-yellow-400"
+                      }`}
+                    />
+                  ))}
+                  <span className="text-sm ml-2 text-gray-500">
+                    ({proj.average_score})
+                  </span>
+
+                  {token !== "" && proj.userReview?.reviewed ? (
+                    <span className="ml-auto text-sm font-semibold text-green-700">
+                      Anda telah mereview, score: {proj.userReview.score}
+                    </span>
+                  ) : null}
+
+                  {/* Show button if user has token and has not reviewed */}
+                  {token !== "" && !proj.userReview?.reviewed && (
+                    <div className="ml-auto">
+                      <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <Button
+                            variant="default"
+                            className="text-sm px-3 py-1"
+                            onClick={() => {
+                              setSelectedProjectId(proj.id_project);
+                              setRating(0);
+                            }}
+                          >
+                            Bintangi
+                          </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>Beri Penilaian</AlertDialogTitle>
+                            <AlertDialogDescription>
+                              Silakan berikan rating untuk proyek ini.
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+
+                          <div className="flex justify-center gap-2 py-4">
+                            {[...Array(5)].map((_, i) => (
+                              <Star
+                                key={i}
+                                onClick={() => setRating(i + 1)}
+                                className={`w-6 h-6 cursor-pointer transition ${
+                                  i < rating
+                                    ? "fill-yellow-400 stroke-yellow-400"
+                                    : "stroke-yellow-400"
+                                }`}
+                              />
+                            ))}
+                          </div>
+
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Batal</AlertDialogCancel>
+                            <AlertDialogAction onClick={() => handleSubmit()}>
+                              Kirim
+                            </AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
